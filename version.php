df413660636e5150e88f51a8a9401c3a84c75043
Alonso Arias <56093780+alonsoarias@users.noreply.github.com>
2024-03-29 11:25:00 -0500

    Update version.php

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2022021804;
+$plugin->version   = 2022021805;
 $plugin->requires  = 2018051700;
 $plugin->component = 'enrol_payumoney';
 $plugin->maturity  = MATURITY_STABLE;
