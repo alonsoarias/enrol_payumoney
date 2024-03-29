b324516d466b9051e7311e227fa3e03cf2f412fd
Alonso Arias <56093780+alonsoarias@users.noreply.github.com>
2024-03-28 19:06:33 -0500

    Update enrol_payumoney.php

--- lang/es/enrol_payumoney.php
+++ lang/es/enrol_payumoney.php
@@ -1,111 +1,107 @@
 <?php
 // Este archivo es parte de Moodle - http://moodle.org/
 //
-// Moodle es un software gratuito: usted puede redistribuirlo y/o modificarlo
+// Moodle es software libre: usted puede redistribuirlo y/o modificarlo
 // bajo los términos de la Licencia Pública General GNU publicada por
-// la Free Software Foundation, ya sea la versión 3 de la Licencia, o
+// la Fundación Free Software; ya sea la versión 3 de la Licencia, o
 // (a su elección) cualquier versión posterior.
 //
 // Moodle se distribuye con la esperanza de que sea útil,
 // pero SIN NINGUNA GARANTÍA; incluso sin la garantía implícita de
-// COMERCIABILIDAD o IDONEIDAD PARA UN PROPÓSITO PARTICULAR.
-// Vea la Licencia Pública General de GNU para más detalles.
+// COMERCIALIZACIÓN o IDONEIDAD PARA UN PROPÓSITO PARTICULAR.
+// Vea la Licencia Pública General GNU para más detalles.
 //
-// Debería haber recibido una copia de la Licencia Pública General de GNU
-// junto con Moodle. Si no es así, consulte <http://www.gnu.org/licenses/>.
+// Debería haber recibido una copia de la Licencia Pública General GNU
+// junto con Moodle.  Si no, consulte <http://www.gnu.org/licenses/>.
 
 /**
  * Cadenas de idioma.
  *
- * Este archivo lista las cadenas de idioma relacionadas con tool_untoken_oauth2.
+ * Este archivo lista todas las cadenas de idioma relacionadas con el plugin de inscripción de PayU Money.
  *
- * @package enrol_payumoney
- * @copyright 2019 Jonathan Lopez <asesor@innovandoweb.com>
- * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 o posterior
+ * @package    enrol_payumoney
+ * @copyright  2019 Jonathan Lopez <asesor@innovandoweb.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+
 $string['pluginname'] = 'PayU Money';
-$string['pluginname_desc'] = 'El módulo PayU Money te permite configurar cursos de pago. Si el costo de algún curso es cero, entonces no se pide a los estudiantes que paguen para entrar. Hay un costo a nivel de sitio que estableces aquí como un valor predeterminado para todo el sitio y luego una configuración de curso que puedes establecer para cada curso individualmente. El costo del curso anula el costo del sitio.';
-$string['owner'] = 'innovandoweb.com';
-$string['descriptionower'] = 'PayU Money desarrollado y mantenido por innovandoweb.com';
-$string['nocost'] = 'No hay valor definido, por favor actualiza el valor o cambia el método de inscripción';
-$string['merchantid'] = 'ID de comerciante';
-$string['accountid'] = 'ID de cuenta';
+$string['pluginname_desc'] = 'El módulo de PayU Money te permite configurar cursos de pago. Si el costo de cualquier curso es cero, entonces a los estudiantes no se les pide que paguen para entrar. Hay un costo a nivel de sitio que estableces aquí como un valor predeterminado para todo el sitio y luego una configuración de curso que puedes establecer para cada curso individualmente. El costo del curso anula el costo del sitio.';
+$string['nocost'] = 'No se ha definido ningún valor, por favor actualiza el valor o cambia el método de inscripción';
+$string['merchantid'] = 'ID de Comerciante';
+$string['accountid'] = 'ID de Cuenta';
 $string['tax'] = 'Impuesto $';
-$string['paycourse'] = 'Este curso requiere un pago para ingresar.';
-$string['merchantapi'] = 'API de comerciante';
-$string['merchantkey'] = 'Clave de comerciante';
-$string['merchantsalt'] = 'Sal de transacción';
+$string['paycourse'] = 'Este curso requiere pago para ingresar.';
+$string['merchantapi'] = 'API de Comerciante';
+$string['merchantkey'] = 'Clave de Comerciante';
+$string['merchantsalt'] = 'Clave de Transacción';
 $string['urlprod'] = 'URL de PayU WebCheckout';
 $string['mailadmins'] = 'Notificar a los administradores';
 $string['mailstudents'] = 'Notificar a los estudiantes';
 $string['mailteachers'] = 'Notificar a los profesores';
-$string['expiredaction'] = 'Acción de vencimiento de la inscripción';
-$string['expiredaction_help'] = 'Selecciona la acción a realizar cuando la inscripción del usuario expire. Ten en cuenta que algunos datos y configuraciones de usuario se eliminan del curso durante la desinscripción del curso.';
-$string['cost'] = 'Costo de inscripción';
+$string['expiredaction'] = 'Acción de expiración de la inscripción';
+$string['expiredaction_help'] = 'Selecciona la acción a realizar cuando expire la inscripción del usuario. Ten en cuenta que algunos datos y configuraciones del usuario se eliminan del curso durante la desinscripción del curso.';
+$string['cost'] = 'Costo de la inscripción';
 $string['costerror'] = 'El costo de la inscripción no es numérico';
-$string['costorkey'] = 'Por favor elige uno de los siguientes métodos de inscripción.';
+$string['costorkey'] = 'Por favor, elige uno de los siguientes métodos de inscripción.';
 $string['currency'] = 'Moneda';
 $string['assignrole'] = 'Asignar rol';
-$string['defaultrole'] = 'Asignación de rol predeterminado';
+$string['defaultrole'] = 'Asignación de rol predeterminada';
 $string['defaultrole_desc'] = 'Selecciona el rol que se debe asignar a los usuarios durante las inscripciones de PayUMoney';
 $string['enrolenddate'] = 'Fecha de finalización';
-$string['enrolenddate_help'] = 'Si está habilitado, los usuarios pueden inscribirse hasta esta fecha solamente.';
+$string['enrolenddate_help'] = 'Si está habilitado, los usuarios pueden inscribirse hasta esta fecha únicamente.';
 $string['enrolenddaterror'] = 'La fecha de finalización de la inscripción no puede ser anterior a la fecha de inicio';
 $string['enrolperiod'] = 'Duración de la inscripción';
-$string['enrolperiod_desc'] = 'Duración predeterminada de tiempo que la inscripción es válida. Si se establece en cero, la duración de la inscripción será ilimitada por defecto.';
-$string['enrolperiod_help'] = 'Duración de tiempo que la inscripción es válida, comenzando desde el momento en que el usuario se inscribe. Si está deshabilitado, la duración de la inscripción será ilimitada.';
+$string['enrolperiod_desc'] = 'Duración predeterminada de tiempo durante el cual la inscripción es válida. Si se establece en cero, la duración de la inscripción será ilimitada por defecto.';
+$string['enrolperiod_help'] = 'Duración de tiempo durante la cual la inscripción es válida, comenzando desde el momento en que el usuario se inscribe. Si está deshabilitado, la duración de la inscripción será ilimitada.';
 $string['enrolstartdate'] = 'Fecha de inicio';
-$string['enrolstartdate_help'] = 'Si está habilitado, los usuarios pueden inscribirse a partir de esta fecha solamente.';
-$string['expiredaction'] = 'Acción de vencimiento de la inscripción';
-$string['expiredaction_help'] = 'Selecciona la acción a realizar cuando la inscripción del usuario expire. Ten en cuenta que algunos datos y configuraciones de usuario se eliminan del curso durante la desinscripción del curso.';
+$string['enrolstartdate_help'] = 'Si está habilitado, los usuarios pueden inscribirse a partir de esta fecha únicamente.';
+$string['expiredaction'] = 'Acción de expiración de la inscripción';
+$string['expiredaction_help'] = 'Selecciona la acción a realizar cuando expire la inscripción del usuario. Ten en cuenta que algunos datos y configuraciones del usuario se eliminan del curso durante la desinscripción del curso.';
 $string['payumoney:config'] = 'Configurar instancias de inscripción de PayUMoney';
-$string['payumoney:manage'] = 'Administrar usuarios inscritos';
+$string['payumoney:manage'] = 'Gestionar usuarios inscritos';
 $string['payumoney:unenrol'] = 'Desinscribir usuarios del curso';
-$string['payumoney:unenrolself'] = 'Desinscribirse del curso (estudiante)';
+$string['payumoney:unenrolself'] = 'Desinscribirme del curso (estudiante)';
 $string['status'] = 'Permitir inscripciones de PayUMoney';
-$string['status_desc'] = 'Permitir que los usuarios utilicen PayUMoney para inscribirse en un curso de forma predeterminada.';
-$string['unenrolselfconfirm'] = '¿Realmente deseas desinscribirte del curso "{$a}"?';
-$string['errorinsert'] = 'No se pudo insertar el registro en PayU';
-
+$string['status_desc'] = 'Permite que los usuarios utilicen PayUMoney para inscribirse en un curso de forma predeterminada.';
+$string['unenrolselfconfirm'] = '¿Realmente quieres desinscribirte del curso "{$a}"?';
+$string['errorinsert'] = 'No se puede insertar el registro en PayUMoney';
 $string['privacy:metadata:enrol_payumoney:payu_money:item_name'] = 'Descripción';
 $string['privacy:metadata:enrol_payumoney:payu_money:courseid'] = 'ID del curso';
 $string['privacy:metadata:enrol_payumoney:payu_money:userid'] = 'ID de usuario';
-$string['privacy:metadata:enrol_payumoney:payu_money:instanceid'] = 'ID de instancia';
+$string['privacy:metadata:enrol_payumoney:payu_money:instanceid'] = 'ID de la instancia';
 $string['privacy:metadata:enrol_payumoney:payu_money:amount'] = 'Cantidad';
 $string['privacy:metadata:enrol_payumoney:payu_money:tax'] = 'Impuesto';
 $string['privacy:metadata:enrol_payumoney:payu_money:paymen_status'] = 'Estado del pago';
 $string['privacy:metadata:enrol_payumoney:payu_money:trans_id'] = 'ID de transacción';
 $string['privacy:metadata:enrol_payumoney:payu_money:payment_id'] = 'ID de pago';
-$string['privacy:metadata:enrol_payumoney:payu_money:auth_json'] = 'JSON de autorización';
-$string['privacy:metadata:enrol_payumoney:payu_money:timeupdated'] = 'Tiempo actualizado';
+$string['privacy:metadata:enrol_payumoney:payu_money:auth_json'] = 'JSON de autenticación';
+$string['privacy:metadata:enrol_payumoney:payu_money:timeupdated'] = 'Hora de actualización';
 $string['privacy:metadata:enrol_payumoney:enrol_payumoney'] = 'Información de la base de datos';
-$string['privacy:metadata:enrol_payumoney:enrol_payumoney:item_name'] = 'Fecha del ítem';
+$string['privacy:metadata:enrol_payumoney:enrol_payumoney:item_name'] = 'Fecha del artículo';
 $string['privacy:metadata:enrol_payumoney:enrol_payumoney:courseid'] = 'ID del curso';
 $string['privacy:metadata:enrol_payumoney:enrol_payumoney:userid'] = 'ID de usuario';
-$string['privacy:metadata:enrol_payumoney:enrol_payumoney:instanceid'] = 'ID de instancia';
+$string['privacy:metadata:enrol_payumoney:enrol_payumoney:instanceid'] = 'ID de la instancia';
 $string['privacy:metadata:enrol_payumoney:enrol_payumoney:amount'] = 'Cantidad';
 $string['privacy:metadata:enrol_payumoney:enrol_payumoney:tax'] = 'Impuesto';
 $string['privacy:metadata:enrol_payumoney:enrol_payumoney:payment_status'] = 'Estado del pago';
 $string['privacy:metadata:enrol_payumoney:enrol_payumoney:trans_id'] = 'ID de transacción';
 $string['privacy:metadata:enrol_payumoney:enrol_payumoney:payment_id'] = 'ID de pago';
-$string['privacy:metadata:enrol_payumoney:enrol_payumoney:auth_json'] = 'JSON de autorización';
-$string['privacy:metadata:enrol_payumoney:enrol_payumoney:timeupdated'] = 'Tiempo actualizado';
+$string['privacy:metadata:enrol_payumoney:enrol_payumoney:auth_json'] = 'JSON de autenticación';
+$string['privacy:metadata:enrol_payumoney:enrol_payumoney:timeupdated'] = 'Hora de actualización';
 $string['privacy:metadata:enrol_payumoney:payu_money'] = 'La extensión de PayU Money envía información al sitio web de PayU Money.';
 $string['paymentconfirm'] = 'Resumen del curso pagado: <div id="resume"><br>Curso: "{$a->item_name}"<br>Valor: "{$a->amount}"<br>Estado: "{$a->payment_status}"<br>Impuesto: "{$a->tax}"<br></div>';
-$string['paymentsorry'] = '¡Gracias por tu pago! Desafortunadamente, tu pago aún no ha sido procesado completamente y aún no estás registrado para ingresar al curso "{$a->fullname}". Pero si continúas teniendo problemas, por favor informa a {$a->teacher} o al administrador del sitio.<br>Estado del pago: "{$a->payment_status}"';
-
+$string['paymentsorry'] = '¡Gracias por tu pago! Desafortunadamente, tu pago aún no ha sido procesado completamente y aún no estás registrado para ingresar al curso "{$a->fullname}". Pero si continúas teniendo problemas, por favor avisa al {$a->teacher} o al administrador del sitio.<br>Estado del pago: "{$a->payment_status}"';
 $string['messageprovider:payumoney_enrolment'] = 'Inscripción de usuario';
-$string['processexpirationstask'] = 'Proceso de vencimiento para PayU';
+$string['processexpirationstask'] = 'Procesar expiraciones para PayU';
 $string['syncenrolmentstask'] = 'Tarea de sincronización de inscripciones de PayU';
 $string['mat'] = 'Sección de configuración';
 $string['mat_desc'] = 'Configuración y mantenimiento';
-$string['clean'] = 'Revisión de base de datos vacía';
-$string['clean_desc'] = 'Para que los registros de pago ocurran solo una vez, se genera un repositorio de datos temporales. Marcar esta casilla limpiará los registros temporales y, al ejecutar el complemento, se realizarán inscripciones nuevamente en función del período elegido en el ítem (periodmp).';
-$string['managediscounts'] = 'Administrar descuentos';
+$string['clean'] = 'Comprobación de base de datos vacía';
+$string['clean_desc'] = 'Para que los registros de pago ocurran solo una vez, se genera un repositorio de datos temporales. Marcar esta casilla limpiará los registros temporales y, al ejecutar el complemento, se realizarán nuevos registros basados en el período elegido en el artículo (periodmp).';
+$string['managediscounts'] = 'Gestionar descuentos';
 $string['viewreports'] = 'Ver informes';
-// Descripciones adicionales para las capacidades
-$string['managediscounts_desc'] = 'Permite a los usuarios administrar descuentos para las tarifas de inscripción en cursos.';
-$string['viewreports_desc'] = 'Permite a los usuarios ver informes de pagos e inscripciones.';
+$string['managediscounts_desc'] = 'Permitir a los usuarios gestionar descuentos para las tasas de inscripción en los cursos.';
+$string['viewreports_desc'] = 'Permitir a los usuarios ver informes de pago e inscripción.';
 $string['payment_id'] = 'ID de Pago';
 $string['fullname'] = 'Nombre Completo';
 $string['email'] = 'Correo Electrónico';
@@ -113,18 +109,14 @@ $string['amount'] = 'Monto Pagado';
 $string['tax'] = 'Impuesto';
 $string['payment_status'] = 'Estado del Pago';
 $string['payment_date'] = 'Fecha del Pago';
-// Agrega cadenas de idioma para campos adicionales aquí, si es necesario.
-$string['reporttitle'] = 'Reporte de Transacciones PayU';
-$string['reportheading'] = 'Detalle de Transacciones PayU';
-
-$string['report'] = 'Informe';
-
-// Archivo: enrol_payumoney.php
-$string['selectformat'] = 'Selecciona el formato';
+$string['reporttitle'] = 'Informe de Transacciones de PayU';
+$string['reportheading'] = 'Detalles de las Transacciones de PayU';
+$string['selectformat'] = 'Seleccionar formato';
 $string['downloadexcel'] = 'Descargar en formato Excel';
 $string['downloadods'] = 'Descargar en formato OpenOffice';
 $string['downloadtext'] = 'Descargar en formato de texto';
 $string['download'] = 'Descargar';
-// en/enrol_payumoney.php
-
 $string['norecords'] = 'No hay registros disponibles';
+$string['discountname'] = 'Nombre del Descuento';
+$string['discountvalue'] = 'Valor del Descuento';
+?>
